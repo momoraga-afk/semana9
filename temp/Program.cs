41700b95d2bc4cff4ae4eb6d889b1f83d0de89bb
MoisesMoraga <momoraga@uamv.edu.ni>
2026-05-11 11:31:05 -0700

    Ejercicios en clase

---
+++ temp/Program.cs
@@ -0,0 +1,26 @@
+﻿double [] temp1 = new double [7];
+double [] tempPromedio = new double [7];
+
+Console.WriteLine("Inserte las temperaturas de la semana");
+for (int i = 0; i < temp1.Length; i++)
+try
+{
+    Console.WriteLine($"Día {i + 1}:");
+    temp1[i] = Convert.ToDouble(Console.ReadLine());  
+}
+catch (FormatException)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Entrada no válida. Por favor, ingrese un número.");
+    Console.ResetColor();
+    i--;
+}
+double suma = 0;
+for (int i = 0; i < temp1.Length; i++) 
+{
+    suma += temp1[i];
+}
+double promedio = suma / temp1.Length;
+Console.WriteLine($"La temperatura promedio de la semana es: {promedio}");
+Console.WriteLine("Las temperaturas más alta es: " + temp1.Max());
+Console.WriteLine("Las temperaturas más baja es: " + temp1.Min());
---
+++ ventasTienda/Program.cs
@@ -0,0 +1,35 @@
+﻿        const int NUM_DIAS = 7; // Número de días a registrar
+        double[] ventas = new double[NUM_DIAS];
+        double totalVendido = 0;
+        int diasSuperaron150 = 0;
+        double ventaMasBaja = double.MaxValue;
+
+        for (int i = 0; i < NUM_DIAS; i++)
+        try
+        {  
+                        Console.Write($"Ingrese las ventas del día {i + 1}: ");
+            ventas[i] = Convert.ToDouble(Console.ReadLine());
+
+            totalVendido += ventas[i];
+
+            if (ventas[i] > 150)
+            {
+                diasSuperaron150++;
+            }
+
+            if (ventas[i] < ventaMasBaja)
+            {
+                ventaMasBaja = ventas[i];
+            } 
+        }
+        catch (FormatException)
+        {
+             Console.ForegroundColor = ConsoleColor.Red;
+             Console.WriteLine("Valor no válido. Por favor ingrese un número entero");
+             Console.ResetColor();
+             i--;
+        }       
+
+        Console.WriteLine($"\nTotal vendido: {totalVendido}");
+        Console.WriteLine($"Número de días con ventas superiores a 150: {diasSuperaron150}");
+        Console.WriteLine($"Venta más baja registrada: {ventaMasBaja}");
